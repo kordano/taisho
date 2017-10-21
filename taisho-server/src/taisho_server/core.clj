cf236dbaca92b6532c1a80f4861dd94e485b02cf
Konrad Kühne <konnykuehne@gmail.com>
2017-10-21 13:01:58 +0200

    add basic trello API connection

--- taisho-server/src/taisho_server/core.clj
+++ taisho-server/src/taisho_server/core.clj
@@ -1,6 +1,23 @@
-(ns taisho-server.core)
+(ns taisho-server.core
+  (:require [org.httpkit.client :as http]
+            [cheshire.core :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def trello-access-params (read-string (slurp "credentials.edn")))
+
+(defn json-get [endpoint]
+  (-> @(http/get (str "https://api.trello.com/1" endpoint)
+                 {:query-params trello-access-params})
+      :body
+      (json/parse-string true)))
+
+(comment
+
+  (def members (json-get "/boards/wF8wnpha/members"))
+
+  (def board (->> (json-get "/boards/wF8wnpha/lists")
+                 (map (fn [{:keys [name id]
+                            :as   tlist}]
+                        (assoc tlist :cards (json-get (str "/lists/" id "/cards")))))))
+
+
+  )
